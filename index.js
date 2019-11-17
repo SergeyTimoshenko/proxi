e2fa2975199e3b3b6c60a71646dc8b4227313f05
Sergey <timoshenko951@gmail.com>
2019-11-17 12:36:38 +0300

    init

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const http = require('http')
+
+var app = express();
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+app.listen(80, function () {
+  console.log('Example app listening on port 3000!');
+});
